42da5f6edbd34069942fbf62ff6fa28c9658cc04
shinil_project <thauf23@naver.com>
2020-03-20 12:34:43 +0900

    js visual img_icon end

--- js/sinipolytec_main.js
+++ js/sinipolytec_main.js
@@ -1,7 +1,17 @@
 $(function(){
 //ready
 
+$('.visual_icon span').on('click',function(){
+    var idx = $(this).index();
+    var idxImg = idx - 2;
 
+    //img_icon Selection
+    if(idx > 1 && idx < 7){
+    $('.visual ul li').removeClass('visual_show');
+    $('.visual ul li').eq(idxImg).addClass('visual_show');
+    }
+
+});
 
 
 
